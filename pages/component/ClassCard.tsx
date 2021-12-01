44ca6f2853bfd24a286886234c0d80ea4e08324f
Matthew Di Marco <mdm@powerledger.io>
2021-12-01 09:36:35 +0800

    Added ability to select classes from grid list

--- pages/component/ClassCard.tsx
+++ pages/component/ClassCard.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
+import { css } from '@emotion/css';
 
 const Card = styled.div`
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
   border-radius: 5px;
-  transition: 0.3s;
+  transition: 0.2s;
   width: 300px;
   padding: 24px;
 
   :hover {
     box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    transform: scale(1.05);
   }
 `
 
@@ -40,19 +42,28 @@ interface ClassCardProps {
   propClassName: string;
   propClassHitDie: number;
   propClassEndpoint: string;
+  propSelected: boolean;
+  propHandleClick: () => void
 };
 
 const ClassCard: React.FC<ClassCardProps> = ({
   propClassName,
   propClassHitDie,
-  propClassEndpoint
+  propClassEndpoint,
+  propSelected,
+  propHandleClick
 }) => {
   const [className, setClassName] = useState<string>(propClassName);
   const [classHitDie, setHitDie] = useState<number>(propClassHitDie);
   const [classEndpoint, setClassEndpoint] = useState<string>(propClassEndpoint);
 
   return (
-    <Card>
+    <Card
+      onClick={propHandleClick}
+      className={css`
+        background: ${propSelected ? css`#f1f1f1;` : css`0`};
+      `}
+    >
       <CardClassHitDue><p>{classHitDie ? classHitDie : -1}</p></CardClassHitDue>
       <CardClassName>{className ? className : 'class name undefined'}</CardClassName>
       <CardClassUrl>{classEndpoint}</CardClassUrl>
--- pages/component/ClassSelect.tsx
+++ pages/component/ClassSelect.tsx
@@ -8,7 +8,7 @@ const ContainerList = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: repeat(auto-fill);
-  grid-gap: 16px;
+  grid-gap: 24px;
   padding: 16px;
   max-height: 90vh;
 `
@@ -20,18 +20,27 @@ interface ClassSelectProps {
   propClassList: ClassSnapshot[];
 };
 
-const ClassSelect: React.FC<ClassSelectProps> = ({ propClassList }) => {
+const ClassSelect: React.FC<ClassSelectProps> = ({ 
+  propClassList,
+}) => {
   const [classList, setClassList] = useState<ClassSnapshot[]>(propClassList);
+  const [classSelectedIdx, setClassSelectedIdx] = useState<number>(-1);
+
+  const handleSelectionChange = (updatedIdx: number) => {
+    setClassSelectedIdx(updatedIdx);
+  };
 
   return (
     <ContainerList>
-      {classList ? classList.map(cls => {
+      {classList ? classList.map((cls, idx) => {
         return (
-          <Child>
+          <Child key={idx}>
             <ClassCard
               propClassName={cls.className}
               propClassHitDie={cls.classHitDie}
               propClassEndpoint={cls.classEndpoint}
+              propSelected={classSelectedIdx === idx ? true : false}
+              propHandleClick={() => handleSelectionChange(idx)}
             />
           </Child>
         );  
